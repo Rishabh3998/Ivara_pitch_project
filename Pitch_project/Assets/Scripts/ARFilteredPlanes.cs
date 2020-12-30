90b98078f7482f3007684b52766230496f0dfd24
Rishabh kumar <rsk1998200@gmail.com>
2020-12-31 04:47:34 +0530

    Plane detection Completed

---
+++ Pitch_project/Assets/Scripts/ARFilteredPlanes.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Linq;
+
+public class ARFilteredPlanes : MonoBehaviour
+{
+
+    public event Action OnVericalPlane;
+    public event Action OnHorizontalPlane;
+
+    private ARPlaneManager arPlaneManager;
+
+    private List<ARPlane> arPlanes;
+
+    private void onEnable(){
+        arPlanes = new List<ARPlane>();
+        arPlaneManager = FindObjectOfType<ARPlaneManager>();
+        arPlaneManager.planesChanged += OnPlanesChanged;
+
+    }
+
+    private void onDisable(){
+        arPlaneManager.planesChanged -= OnPlanesChanged;
+
+    }
+
+    private void OnPlanesChanged(ARPlanesChangedEventArgs args){
+        if(args.added != null && args.added.Count > 0)
+            arPlanes.AddRange(args.added);
+
+        foreach (ARPlane plane in arPlanes.Where(plane => plane.extents.x * plane.extents.y >= 0.1f))
+        {
+            if(plane.alignment.IsVertical()){
+                OnVericalPlane.Invoke();
+            }
+            else{
+                OnHorizontalPlane.Invoke();
+            }
+        }
+        
+    }
+}
---
+++ Pitch_project/Assets/Scripts/FilteredPlanesCanvas.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FilteredPlanesCanvas : MonoBehaviour
+{
+    [SerializeField] private Toggle verticalPlaneToggle;
+    [SerializeField] private Toggle horizontalPlaneToggle;
+    [SerializeField] private Button startButton;
+
+    private ARFilteredPlanes arFilteredPlanes;
+
+    public bool VerticalPlaneToggle { 
+        get => verticalPlaneToggle.isOn;
+        set{
+            verticalPlaneToggle.isOn = value;
+            CheckIfAllareTrue();
+        }
+    }
+
+    public bool HorizontalPlaneToggle { 
+        get => horizontalPlaneToggle.isOn;
+        set{
+            horizontalPlaneToggle.isOn = value;
+            CheckIfAllareTrue();
+        }
+    }
+        
+    private void CheckIfAllareTrue()
+    {
+        if(VerticalPlaneToggle || HorizontalPlaneToggle){
+            startButton.interactable = true;
+        }
+    }
+
+    private void onEnable(){
+        arFilteredPlanes = FindObjectOfType<ARFilteredPlanes>();
+
+        arFilteredPlanes.OnVericalPlane += () => VerticalPlaneToggle = true;
+        arFilteredPlanes.OnHorizontalPlane += () => HorizontalPlaneToggle = true;
+    }
+    private void onDisable(){
+
+        arFilteredPlanes.OnVericalPlane -= () => VerticalPlaneToggle = true;
+        arFilteredPlanes.OnHorizontalPlane -= () => HorizontalPlaneToggle = true;
+
+    }
+
+    
+}
