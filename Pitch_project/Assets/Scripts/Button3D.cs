028ab72cfe38bd835c562f9620cc16503ee4bd36
Rishabh kumar <rsk1998200@gmail.com>
2021-01-01 04:08:29 +0530

    First Slide Working done

---
+++ Pitch_project/Assets/Scripts/Button3D.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button3D : MonoBehaviour
+{
+    public void buttonFunction(string btnLink){
+        Application.OpenURL(btnLink);
+    }
+}
