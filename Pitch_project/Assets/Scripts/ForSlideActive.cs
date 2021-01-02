04cc21b93a7635b9d76c99cd23c8f12c092f2ee4
Rishabh kumar <rsk1998200@gmail.com>
2021-01-02 20:37:49 +0530

    first stage working/completed

---
+++ Pitch_project/Assets/Scripts/ForSlideActive.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForSlideActive : MonoBehaviour
+{
+    public GameObject myobject;
+    public bool activateme;
+    //private Vector2 touchPosition;
+    
+    void Start(){
+        myobject.SetActive(false);
+    }
+    
+    // bool ForActivation(){
+    //     if(Input.touchCount > 0){
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    /*public GameObject SphereObject;
+
+
+    void Update () {
+    if (Input.touchCount > 0) {
+    if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+
+    SphereObject.SetActive (true);
+
+    }
+
+    }
+    */
+    
+    void Update()
+    {
+        if(Input.touchCount > 0){
+            if(Input.GetTouch(0).phase == TouchPhase.Ended){
+                myobject.SetActive(true);
+            }
+        }
+    }
+}
