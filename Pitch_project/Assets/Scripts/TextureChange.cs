a92571f22a843d3e6488178f6bbbfb647fcb3760
Rishabh kumar <rsk1998200@gmail.com>
2021-01-03 09:32:13 +0530

    prebuild completed

---
+++ Pitch_project/Assets/Scripts/TextureChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public Material[] material;
+    public int x;
+    Renderer rend;
+    
+    void Start()
+    {
+        x = 0;
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[x];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.sharedMaterial = material[x];
+    }
+
+    public void NextColor(){
+        if(x<19){
+            x++;
+        }
+        else{
+            x=0;
+        }
+    }
+}
